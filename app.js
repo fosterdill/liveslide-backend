0cf9f3ee28aed948d434f0bc8276b27436b87159
Dylan Foster <fosterdill@gmail.com>
2014-03-12 20:04:46 -0700

    add mongodb support

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var MongoClient = require('mongodb').MongoClient;
+var testStuff = 'hi';
 var app = express();
 
 // all environments
@@ -28,9 +29,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+MongoClient.connect('mongodb://127.0.0.1:27017/liveslide', function(err, db) {
+  app.get('/', routes.index(db));
+  app.get('/users', user.list);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
+
   console.log('Express server listening on port ' + app.get('port'));
 });
